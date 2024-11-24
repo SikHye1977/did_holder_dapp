f35ce83a8c1baca655dc6f536244b0714c07c39f
SikHye1977 <sikhye1977@naver.com>
2024-11-24 19:02:05 +0900

    first commit

---
+++ src/screen/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {SafeAreaView, StyleSheet, Text} from 'react-native';
+
+function Home() {
+  return (
+    <SafeAreaView>
+      <Text></Text>
+    </SafeAreaView>
+  )
+}
+
+const styles = StyleSheet.create({});
+
+export default Home;
